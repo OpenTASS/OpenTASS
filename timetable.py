9d777b8bdd946c9d07704949f2cb212226d4df55
Capta1nT0ad <capta1nt0adyt@gmail.com>
2024-05-09 08:17:28 +1000

    return raw python timetable data as well

--- timetable.py
+++ timetable.py
@@ -109,7 +109,7 @@ def get_day_timetable(
         df["Year Group"] = df["Year Group"].astype(int)
         df["Year Group"] = df["Year Group"].replace(-1, "")
 
-        return df, df.to_html(index=False), site_data["HEADERLABEL"]
+        return df, df.to_html(index=False), site_data["HEADERLABEL"], site_data
 
     else:
         logging.fatal(
