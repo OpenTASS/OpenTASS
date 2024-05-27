74ca36d371f5a129037ebb96e5fcfbe53d75eb06
Capta1nT0ad <capta1nt0adyt@gmail.com>
2024-05-27 13:56:22 +1000

    Replace lengthy license notice with a proper SPDX id

--- src/opentass/__init__.py
+++ src/opentass/__init__.py
@@ -1,11 +1,2 @@
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# OpenTASS is a Python library providing API access to The Alpha School System. #
-# Copyright (C) 2024 OpenTASS (capta1nt0ad and MrPiggy105)                      #
-#                                                                               #
-# OpenTASS is free software: you can redistribute it and/or modify              #
-# it under the terms of the GNU General Public License as published by          #
-# the Free Software Foundation, either version 3 of the License, or             #
-# (at your option) any later version.                                           #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-# Nothing here yet!
+# SPDX-License-Identifier: GPL-3.0-or-later
+# __init__.py is required for the Python packaging specification.
--- src/opentass/academic_reports.py
+++ src/opentass/academic_reports.py
@@ -1,12 +1,4 @@
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# OpenTASS is a Python library providing API access to The Alpha School System. #
-# Copyright (C) 2024 OpenTASS (capta1nt0ad and MrPiggy105)                      #
-#                                                                               #
-# OpenTASS is free software: you can redistribute it and/or modify              #
-# it under the terms of the GNU General Public License as published by          #
-# the Free Software Foundation, either version 3 of the License, or             #
-# (at your option) any later version.                                           #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 import logging
 import requests
